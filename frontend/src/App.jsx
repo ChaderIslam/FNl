06d39ce160227bf1f8e521ff592e2435292d3bc7
ChaderIslam <98845571+ChaderIslam@users.noreply.github.com>
2025-09-09 00:14:26 +0100

    v1
    front end of the login page and the dashbord page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Dashboard from './login/screens/Dashboard.jsx'
+import LoginScreen from './login/screens/LoginScreen.jsx'  // Capitalized import
+
+function App() {
+  return (
+    <>
+      <Dashboard />  {/* Capitalized component */}
+    </>
+  )
+}
+
+export default App
